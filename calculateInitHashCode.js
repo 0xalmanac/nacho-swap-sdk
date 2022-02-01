4cf4b47a60d93d473accf215a4a9ad14cebb523e
0xalmanac <0xalmanac@protonmail.com>
2022-02-01 14:08:48 -0800

    And we're back to quickswap

--- calculateInitHashCode.js
+++ calculateInitHashCode.js
@@ -9,6 +9,6 @@ const address = require('@ethersproject/address');
 const tokens1address = "0xcD86152047e800d67BDf00A4c635A8B6C0e5C4c2";
 const tokens0address = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
 
-const pairAddress = address.getCreate2Address("0x758D44fD168861128BeF6C4699950a911d05986b", solidity.keccak256(['bytes'], [solidity.pack(['address', 'address'], [tokens0address, tokens1address])]), COMPUTED_INIT_CODE_HASH);
+const pairAddress = address.getCreate2Address("0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32", solidity.keccak256(['bytes'], [solidity.pack(['address', 'address'], [tokens0address, tokens1address])]), COMPUTED_INIT_CODE_HASH);
 
 console.log(pairAddress);
