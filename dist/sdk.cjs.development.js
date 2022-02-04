2c819448a2510c66237820a26096151174565020
0xalmanac <0xalmanac@protonmail.com>
2022-02-04 01:00:28 -0800

    Build files

--- dist/sdk.cjs.development.js
+++ dist/sdk.cjs.development.js
@@ -45,7 +45,7 @@ var THREE = /*#__PURE__*/JSBI.BigInt(3);
 var FIVE = /*#__PURE__*/JSBI.BigInt(5);
 var TEN = /*#__PURE__*/JSBI.BigInt(10);
 var _100 = /*#__PURE__*/JSBI.BigInt(100);
-var FEES_NUMERATOR = /*#__PURE__*/JSBI.BigInt(997);
+var FEES_NUMERATOR = /*#__PURE__*/JSBI.BigInt(9970);
 var FEES_DENOMINATOR = /*#__PURE__*/JSBI.BigInt(10000);
 var SolidityType;
 
